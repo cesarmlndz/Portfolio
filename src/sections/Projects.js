f4693b8c17ffe13fc5c3c2958c742a0062a6f65f
Cesar Melendez <96844297+cesarmlndz@users.noreply.github.com>
2024-10-16 11:58:53 -0400

    Update Projects.js

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -5,35 +5,21 @@ import unoflowImg from '../assets/unowflowImg.png';
 import wildfireAIImg from '../assets/wildfireAIImg.png';
 import cesarAIImg from '../assets/cesarAIImg.png'
 import goodnewsimg from '../assets/goodnewsimg.png';
-
+import firewatchimg from '../assets/firewatchweb.png';
 
 export default function Projects() {
     const projects = [
         {
-            title: "GoodNews!",
-            desription: "Full-Stack application using the PERN Stack (PostgreSQL, ExpressJS, React, NodeJs). Authentication powered by Firebase. GoodNews! lets users track their job applications in an organized manner, as well view analytics regarding their applications with detailed charts and percentages using ChartJs.",
-            imageURL: goodnewsimg,
-            liveURL: "https://goodnews-bay.vercel.app/"
+            title: "FireWatch",
+            desription: "Led development of both the mobile (React Native) and web (React) versions of a revenue earning, subscription-based, SaaS disaster prevention application. It is aimed at predicting and tracking wildfires using advanced machine learning algorithms, satellite imagery, weather data, and more. The application provides real-time data on active and historical fires, allows users to comment on events and predictions, send out mission critical alerts, and displays information on an interactive 3D map, helping communities stay informed and prepared.",
+            imageURL: firewatchimg,
+            liveURL: "http://space-eyes.info/#/login"
         },
-        {
+         {
             title: "findmyroommate!",
             desription: "Full-Stack React and Firebase web application for college students to find roommates easily. Users can connect with others in need of roommates by posting their housing situation on the site.",
             imageURL: findmyroommateImg,
             liveURL: "https://findmyroommate-b2820.web.app/"
-        },
-        {
-            title: "UNOFLOW WEB3",
-            desription: "React web application that splits into two services: NFT Marketplace & Crypto Tracker. Users can browse, track, and purchase the hottest NFT’s and crypto currencies on the market.",
-            imageURL: unoflowImg,
-            liveURL: "https://github.com/cesarmlndz/UNOFLOW"
-
-        },
-        {
-            title: "Wildfire-AI",
-            desription: "Space-Eyes, the company where I interned, will soon be launching Wildfire-AI. This is a wildfire detection and prediction service using advanced machine learning algorithms. My task was to develop a web application for a landing page using HTML, CSS, JavaScript, ChartJs, and Material UI. ",
-            imageURL: wildfireAIImg,
-            liveURL: "http://wildfire-ai.com/"
-
         },
     ]
 
